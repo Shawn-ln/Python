2734d26a4e7c9abdabba6f2e37de81a5e5acba8a
Shawn <642864989@qq.com>
2021-02-26 11:28:53 +0800

    update UpdatePatch.py,create support.py test.py   --0226

--- support.py
+++ support.py
@@ -1,13 +1,24 @@
 # Filename: support.py
+import os
+import re
 import datetime
 
-def print_func(par):
-    print("Hello : ", par)
-    return
 
-def titles(RUNITEM):
+def gettesttime(start):
+    EndTime = datetime.datetime.now()
+    EndTimes = re.sub(r'\..*$', "", str(EndTime))
+    d1 = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
+    d2 = datetime.datetime.strptime(EndTimes, '%Y-%m-%d %H:%M:%S')
+    TestTimes = d2 - d1
+    return TestTimes
+
+
+def titles(RUNITEM, stage):
     now = datetime.datetime.now()
-    print('title: ' + str(RUNITEM) + ' is start running at ' + str(now))
+    StartTimes = re.sub(r'\..*$', "", str(now))
+    print('title: ' + str(RUNITEM) + ' is ' + str(stage) + ' running at ' + str(StartTimes))
+    return StartTimes
+
 
 def judge(FailRetry, FailRetrytimes):
     if int(FailRetry) < int(FailRetrytimes):
@@ -15,3 +26,61 @@ def judge(FailRetry, FailRetrytimes):
     return False
 
 
+def setinfo(RUNITEM, SN, UPLIMIT, LOWLIMIT, Result, NUM, LOGINFO, Starttime, TestTime):
+    global PASSFAIL
+    if Result == 'P':
+        PASSFAIL = 'PASS'
+    if Result == 'F':
+        PASSFAIL = 'FAIL'
+    os.chdir(r'C:\WinTest\LogFile')
+    logname = str('TestLog_' + PASSFAIL + '.txt')
+    with open(logname, 'a', encoding='utf-8') as f:
+        f.write('PAT_TEST,' + RUNITEM + ',' + SN + ',' +
+                UPLIMIT + ',' + LOWLIMIT +
+                ',' + PASSFAIL + ',' + NUM + ',' + LOGINFO + ',' + str(Starttime) + ',' + str(TestTime) + '\n')
+        return
+        # print(f.write('PAT_TEST,' + SN + RUNITEM + UPLIMIT + LOWLIMIT + PASSFAIL + NUM + LOGINFO + TestTime + '\n'))
+
+
+def getSN():
+    os.chdir(r'C:\WinTest\Tools')
+    instruct = 'EEPROM64.exe -g -ln -f SN.txt'
+    os.system(instruct)
+    log_name = 'SN.txt'
+    with open(log_name, 'r', encoding='utf-8', newline='') as f:
+        for line in f:
+            SN = line
+            pattern = re.compile(r'^[0-9A-Z]{8}$')
+            m = pattern.match(SN)
+            print(m.group())  # 匹配成功m.group()才会有值，匹配失败会报错
+    return SN
+
+
+def creatResult(Fixed, ItemName, Result, ItemTag):
+    os.chdir(r'C:\WinTest\TestStand')
+    instruct = 'tasklist | find "TestStand.exe"'
+    rea = os.system(instruct)
+    getID = 'sdtCheckResult.exe /a>' + ItemName + '.txt'
+    os.system(getID)
+    logname = ItemName + '.txt'
+    with open(logname, 'r', encoding='utf-8', newline='') as f:
+        for line in f.readlines():
+            if ItemName in line:
+                str = line
+                print(str)
+    ItemID1 = re.sub(r'\|.*$', '', str)
+    ItemID = re.sub(r'\D', '', ItemID1)
+    # ItemID = str(ItemID2)
+    print(ItemID)
+
+    if rea == 0:
+        para = "%s %s %s %d %s %s" % ("sdtCreateResult.exe", Fixed, ItemName, Result, ItemID, ItemTag)
+        print(para)
+        os.system(para)
+        return 0
+    if rea == 1:
+        ex = Exception("TestStand.exe窗口不存在！！！")
+        # 抛出异常对象
+        raise ex
+    else:
+        print(instruct + ' 语句执行异常')
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import support
+import os
+import time   # 测试时拿掉
 
 # 开头模板信息
 dict = {
@@ -8,29 +10,86 @@ dict = {
     'LOWLIMIT':'-9999.900',
     'RUNITEM':'UpdatePatch',
 }
+try:
+    # currentPath = os.getcwd()
+    currentPath = r'C:\WinTest\Work'
+    print(currentPath)
+    MB_SN = support.getSN()
+    StartTime = support.titles(RUNITEM = dict['RUNITEM'], stage = 'start')
+    for i in dict:
+        print(i + ' : ' + dict[i])
 
-support.titles(dict['RUNITEM'])
-for i in dict:
-    print(i + ' : ' + dict[i])
+    # 测试正文
+    for n in range(1, 200):
+        # 测试内容和结果
+        print('测试正文')
+        if dict['FailRetry'] == '5':
+            result = 'pass'
+        else:
+            result = 'fail'
+        # result = 'fail'
 
-# 测试正文
-for n in range(1, 200):
-    # 测试内容和结果
-    print('\033[32;0m测试正文')
-    if dict['FailRetry'] == '5':
-        result = 'pass'
-    else:
-        result = 'fail'
+        # 判断测试结果
+        if result == 'fail':
+            if support.judge(FailRetry=dict['FailRetry'], FailRetrytimes=dict['FailRetrytimes']):
+                dict['FailRetry'] = str(int(dict['FailRetry']) + 1)
+                print('测试循环次数：' + dict['FailRetry'], '，测试结果：fail！！！')
+                continue
 
-    # 判断测试结果
-    if result == 'fail':
-        if support.judge(FailRetry=dict['FailRetry'], FailRetrytimes=dict['FailRetrytimes']):
-            dict['FailRetry'] = str(int(dict['FailRetry']) + 1)
-            print('测试循环次数：' + dict['FailRetry'], '，测试结果：fail！！！')
-            continue
-        print('测试fail')
-        break
+            TestTimes = support.gettesttime(start=StartTime)
+            print('测试用时:', TestTimes)
+            support.creatResult(Fixed = currentPath, ItemName = dict['RUNITEM'], Result = -1, ItemTag = 0)
+            support.setinfo(RUNITEM=dict['RUNITEM'], SN=MB_SN, UPLIMIT=dict['UPLIMIT'],
+                            LOWLIMIT=dict['LOWLIMIT'], Result='F', NUM='0',
+                            LOGINFO='UpdatePatch Fail', Starttime=StartTime, TestTime=TestTimes)
+            print('测试循环次数:', n, '，测试结果：fail！！！！')
+            break
 
-    if result == 'pass':
-        print('测试循环次数:', n ,'，测试结果：pass！！！')
-        break
+        if result == 'pass':
+            print('测试循环次数:', n, '，测试结果：pass！！！')
+            time.sleep(3)    # 测试时拿掉
+            print('测试SN:', MB_SN)
+            support.creatResult(Fixed = currentPath, ItemName = dict['RUNITEM'], Result = 1, ItemTag = 0)
+            TestTimes = support.gettesttime(start=StartTime)
+            print('测试用时:', TestTimes)
+            support.setinfo(RUNITEM=dict['RUNITEM'], SN=MB_SN, UPLIMIT=dict['UPLIMIT'],
+                            LOWLIMIT=dict['LOWLIMIT'], Result='P', NUM='1',
+                            LOGINFO='UpdatePatch Success', Starttime=StartTime, TestTime=TestTimes)
+            break
+
+        else:
+            print('无测试结果！！！')
+
+
+except AttributeError as e:
+    print(e)
+    print("SN匹配信息错误，请检查正则表达式！！！")
+
+except Exception as e:
+    print(e)
+
+"""
+python中时间日期格式化符号：
+%y 两位数的年份表示（00-99）
+%Y 四位数的年份表示（000-9999）
+%m 月份（01-12）
+%d 月内中的一天（0-31）
+%H 24小时制小时数（0-23）
+%I 12小时制小时数（01-12）
+%M 分钟数（00=59）
+%S 秒（00-59）
+%a 本地简化星期名称
+%A 本地完整星期名称
+%b 本地简化的月份名称
+%B 本地完整的月份名称
+%c 本地相应的日期表示和时间表示
+%j 年内的一天（001-366）
+%p 本地A.M.或P.M.的等价符
+%U 一年中的星期数（00-53）星期天为星期的开始
+%w 星期（0-6），星期天为星期的开始
+%W 一年中的星期数（00-53）星期一为星期的开始
+%x 本地相应的日期表示
+%X 本地相应的时间表示
+%Z 当前时区的名称
+%% %号本身
+"""
